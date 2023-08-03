52e40332a309f04eaf9e018eb011125c104131b0
Deborah <119611300+Deborah-Mokeira@users.noreply.github.com>
2023-08-03 11:34:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,20 @@ const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
-// Show Loading
-function loading(){
+
+function showLoadingSpinner(){
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete(){
+function removeLoadingSpinner(){
   quoteContainer.hidden =false;
   loader.hidden = true;
 }
 
 // Show New Quote
 function newQuote(){
-   loading();
+  showLoadingSpinner;
     // Pick a Random Quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace with 'Unknown'
@@ -41,19 +40,22 @@ function newQuote(){
     }
     // Set Quote, Hide Loader
  quoteText.textContent = quote.text;
- complete();
+ removeLoadingSpinner();
 }
 
 // Get Quotes From API
 async function getQuotes(){
-    loading();
+  showLoadingSpinner();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
       const response = await fetch (apiUrl);
       apiQuotes = await response.json();
       newQuote();
     }catch(error){
-      //Catch Error Here 
+      //Handle any errors that occur during the fetch
+      console.log('Error fetching quote:', error);
+      quoteText.innerText = 'Failed to fetch a quote.';
+      authorText.innerText = '';
     }
 }
 // Tweet a Quote
